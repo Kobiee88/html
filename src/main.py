fb757485196349fd082c99f1e938d321d4008252
Kobiee88 <thomas.mysliwietz@gmail.com>
2025-08-17 23:43:53 +0200

    deployment of /docs folder

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import os
 import shutil
+import sys
 
 from functions import markdown_to_html_node
 
 def main():
-    init_file_copy("./static", "./public")
-    #generate_page("./content/index.md", "./template.html", "./public/index.html")
-    generate_pages_recursive("./content", "./template.html", "./public", "./content")
+    basepath = "/"
+    if sys.argv[1]:
+        basepath = sys.argv[1]
+    init_file_copy("./static", "./docs")
+    generate_pages_recursive("./content", "./template.html", "./docs", "./content", basepath=basepath)
 
 def init_file_copy(source, destination):
     if not os.path.exists(source):
@@ -45,7 +48,7 @@ def extract_title(markdown):
             return line[2:].strip()
     return None
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} using template {template_path} to {dest_path}")
     with open(from_path, 'r') as f:
         markdown_content = f.read()
@@ -54,12 +57,13 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     html_content = markdown_to_html_node(markdown_content).to_html()
     template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    template_content = template_content.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, 'w') as dest_file:
         dest_file.write(template_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, root_content_dir=None):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, root_content_dir=None, basepath=None):
     if root_content_dir is None:
         root_content_dir = dir_path_content
     for item in os.listdir(dir_path_content):
@@ -71,6 +75,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, roo
             dest_path = os.path.join(dest_dir_path, relative_path.replace('.md', '.html'))
             if not os.path.exists(os.path.dirname(dest_path)):
                 os.makedirs(os.path.dirname(dest_path))
-            generate_page(item_path, template_path, dest_path)
+            generate_page(item_path, template_path, dest_path, basepath=basepath)
 
 main()
